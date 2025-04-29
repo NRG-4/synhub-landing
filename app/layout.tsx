c11c0c34b6e9fe41de56ab7ec99722eb3f853c28
NeoRise <defneorise@gmail.com>
2025-04-28 22:59:24 -0500

    chore: project initialization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {ThemeProvider} from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="es" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+      >
+          <div className={`grid min-h-dvh grid-cols-[auto 1fr auto] w-full`}>
+              {children}
+          </div>
+      </ThemeProvider>
       </body>
     </html>
-  );
+);
 }
