347eeace62b90b5c0fb566a39ef939e2d112a043
NeoRise <defneorise@gmail.com>
2025-05-09 12:02:31 -0500

    feat: add main page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import Hero from "@/components/Hero";
+import MisionAndVision from "@/components/MisionAndVision";
+import Features from "@/components/Features";
+import Team from "@/components/Team";
+import CallToContact from "@/components/CallToContact";
 
 export default function Home() {
   return (
       <>
-
+          <Hero/>
+          <Features/>
+          <MisionAndVision/>
+          <Team/>
+          <CallToContact/>
       </>
   );
 }
